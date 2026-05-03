3b6657f3364314de8ae24d6713453a22c60e2282
Ayan Pasha <jordantoronto3@gmail.com>
2026-05-03 03:41:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+// Chapter 4 - Variables for Strings
+
+// Q1
+var productCost = "3.45";
+
+// Q2
+var nameOfBand;
+
+// Q3
+var userName = "Ayan";
+
+// Q4
+var teamName = "Pakistan";
+alert(teamName);
+
+// Q5
+var bestMan = "Charlie";
+
+// Extra Practice Outputs
+
+alert("Welcome " + userName);
+alert("Best Man is " + bestMan);
+alert("Product Cost is $" + productCost);
